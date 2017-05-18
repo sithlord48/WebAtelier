77e0000190ba74a4aef26c86c463b1cd925e1a73
Lays Rodrigues <laysrodriguessilva@gmail.com>
2017-05-18 11:22:11 -0300

    Add blog template
    Also base mockup of 'cards' to build the page
    with contents from atelier contributor's blog.
    Still need to discover the js function to
    do this.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- atelier/atelier.py
+++ atelier/atelier.py
@@ -10,5 +10,14 @@ app.config.from_pyfile('config.cfg', silent=True)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)

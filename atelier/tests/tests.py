ddcc1967c782aba302ef7ea14aa17418b8266817
Lays Rodrigues <laysrodriguessilva@gmail.com>
2017-05-18 13:19:55 -0300

    Add Tests
    For the good in my soul.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

---
+++ atelier/tests/tests.py
@@ -0,0 +1,37 @@
+import unittest
+from atelier import app
+
+class TestView(unittest.TestCase):
+        def setUp(self):
+            app.config['TESTING'] = True
+            self.app = app.test_client()
+
+        def tearDown(self):
+            pass
+
+        def test_sanity(self):
+            self.assertEqual(2, 2)
+
+        def test_home_status_code(self):
+            result = self.app.get('/')
+            self.assertEqual(result.status_code, 200)
+
+        def test_about_status_code(self):
+            result = self.app.get('/about')
+            self.assertEqual(result.status_code, 200)
+
+        def test_blog_status_code(self):
+            result = self.app.get('/blog')
+            self.assertEqual(result.status_code, 200)
+
+        def test_contact_status_code(self):
+            result = self.app.get('/contact')
+            self.assertEqual(result.status_code, 200)
+
+        def test_raise_404(self):
+            result = self.app.get('/jaja')
+            self.assertEqual(result.status_code, 404)
+
+
+if __name__ == '__main__':
+    unittest.main()

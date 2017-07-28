84e94b593c51563e8095c3c9361c40a4f74b12ea
Lays Rodrigues <laysrodriguessilva@gmail.com>
2017-07-28 10:22:05 -0300

    Add 404 and 500 exceptions handlers and templates
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- atelier/atelier.py
+++ atelier/atelier.py
@@ -5,6 +5,16 @@ from flask import Flask, render_template
 #Init Flask
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    print(e)
+    return render_template('404.html')
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    print(e)
+    return render_template('500.html')
+
 @app.route('/')
 def index():
     return render_template('index.html')

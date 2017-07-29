aa1661a6391c6fed32dc3241b0dd9c1cf9c7b499
Lays Rodrigues <laysrodriguessilva@gmail.com>
2017-07-29 00:58:27 -0300

    Minor adjusts on setup.py
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Atelier',
+    name='atelier',
     version='0.1',
     description='Atelier website',
     url='https://github.com/lays147/webatelier',
     author='Lays Rodrigues',
     author_email='laysrodriguessilva@gmail.com',
     license='GPLV3',
-    packages=['atelier'],
+    packages=find_packages(exclude=('tests')),
     install_requires=[
         'Flask==0.12.1',
         # 'gunicorn==19.6.0',

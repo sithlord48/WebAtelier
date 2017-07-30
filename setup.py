8c64f76764a7f29c2fe9e281b73e811962db829d
Lays Rodrigues <laysrodriguessilva@gmail.com>
2017-07-29 22:05:40 -0300

    Update Setup/Tox
    Add stuff to fix the templates not installed on setup install.
    
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author_email='laysrodriguessilva@gmail.com',
     license='GPLV3',
     packages=find_packages(exclude=('tests')),
+    include_package_data=True,
     install_requires=[
         'Flask==0.12.1',
         # 'gunicorn==19.6.0',
